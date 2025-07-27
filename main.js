71756ef20a0db499f464d9ad6f9eb25fe5e40267
marjonakenjayeva <marjonakenjayeva62@gmail.com>
2025-07-28 01:06:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let logo = document.getElementById('logo')
+let a = document.getElementById('a')
+let b = document.getElementById('b')
+let c = document.getElementById('c')
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let title = document.getElementById('title')
+let text = document.getElementById('text')
+let btn3 = document.getElementById('btn3')
+let body = document.getElementById('body')
+
+function DarkMode() {
+logo.style.color = "white"
+a.style.color = "white"
+b.style.color = "white"
+c.style.color = "white"
+btn1.style.background = "#C4B5A5"
+btn1.style.color ="white"
+btn2.style.background = "#FA9021"
+btn2.style.color ="white"
+title.style.color ="white"
+text.style.color ="white"
+btn3.style.color ="white"
+body.style.backgroundColor = "black"
+}
